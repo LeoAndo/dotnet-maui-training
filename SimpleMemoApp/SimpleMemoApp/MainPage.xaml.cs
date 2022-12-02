12521baed0404dc6aecfdd9d8bf2f2105ff62e47
LeoAndo <reo.ando@sun-asterisk.com>
2022-12-02 18:20:41 +0900

    Modelと画面の紐付け

--- SimpleMemoApp/SimpleMemoApp/MainPage.xaml.cs
+++ SimpleMemoApp/SimpleMemoApp/MainPage.xaml.cs
@@ -14,11 +14,21 @@ public partial class MainPage : ContentPage
     void BtnRead_Clicked(System.Object sender, System.EventArgs e)
     {
         Debug.WriteLine("Call BtnRead_Clicked");
+        Memo memo = MemoHolder.Current.Memo;
+        Date.Date = memo.Date;
+        Subject.Text = memo.Subject;
+        Text.Text = memo.Text;
     }
 
     void BtnSave_Clicked(System.Object sender, System.EventArgs e)
     {
         Debug.WriteLine("Call BtnSave_Clicked");
+        MemoHolder.Current.Memo = new Memo()
+        {
+            Date = Date.Date,
+            Subject = Subject.Text,
+            Text = Text.Text,
+        };
     }
 }
 
