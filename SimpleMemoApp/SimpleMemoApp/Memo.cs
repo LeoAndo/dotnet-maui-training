706c54d811fa5bcd0e8a36899eaeabcb97f7830b
LeoAndo <reo.ando@sun-asterisk.com>
2022-12-02 15:20:24 +0900

    メモアプリのModelの実装

---
+++ SimpleMemoApp/SimpleMemoApp/Memo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace SimpleMemoApp
+{
+	public class Memo
+	{
+		public Memo()
+		{
+		
+		}
+		public DateTime Date { get; set; }
+		public string Subject { get; set; }
+		public string Text { get; set; }
+	}
+}
+
