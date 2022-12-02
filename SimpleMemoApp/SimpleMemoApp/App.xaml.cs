1f572f877dbac14a68fd0f5cf41dcc49db781b88
LeoAndo <reo.ando@sun-asterisk.com>
2022-12-02 17:15:32 +0900

    画面のライフサイクルイベントをフックする

--- SimpleMemoApp/SimpleMemoApp/App.xaml.cs
+++ SimpleMemoApp/SimpleMemoApp/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace SimpleMemoApp;
+﻿using System.Diagnostics;
+
+namespace SimpleMemoApp;
 
 public partial class App : Application
 {
@@ -8,5 +10,44 @@ public partial class App : Application
 
 		MainPage = new AppShell();
 	}
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        Window window = base.CreateWindow(activationState);
+
+        // Androidで例えると、
+        window.Created += (s, e) =>
+        {
+            // OnPostCreate
+            Debug.WriteLine("onCreate!");
+        };
+
+        window.Activated += (s, e) =>
+        {
+            // onResume
+            Debug.WriteLine("Activated!");
+        };
+        window.Deactivated += (s, e) =>
+        {
+            // onPause
+            Debug.WriteLine("Deactivated!");
+        };
+        window.Stopped += (s, e) =>
+        {
+            // onStop
+            Debug.WriteLine("onStop!");
+        };
+        window.Resumed += (s, e) =>
+        {
+            // onReStart
+            Debug.WriteLine("onReStart!");
+        };
+        window.Destroying += (s, e) =>
+        {
+            // onDestroy
+            Debug.WriteLine("onDestroy!");
+        };
+        return window;
+    }
 }
 
